9ed84da473b32798c673bdcc9dd3ad652f0c6d2d
Clueless Clown <thanhnguyen@emddi.com>
2023-09-18 17:11:32 +0700

    redis as intermediate message broker
    + each server instance will connect to redis
        + when request comes, publish that message into a topic
        + other server instances subscribe to the topic, it will boardcast
          to all of its connected socket clients
    + the keypoint here: redis will handle the COMMUNICATION FOR EACH SERVER
      INSTANCES

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 const WebSocketServer = require('ws').Server
 const http = require('node:http')
 const ecstatic = require('ecstatic')
+const {createClient} = require('redis')
 
+const client = createClient()
+const sub = client.duplicate()
+const pub = client.duplicate()
+const CHANNEL = 'chat_messages' 
 const server = http.createServer(
     ecstatic( {root: __dirname + '/www'} )
 )
-
 const wss = new WebSocketServer( {server} )
-wss.on('connection', conn => {
-    console.log('a client just connected')
-    conn.on('message', msg => {
-        boardcast(msg.toString())
-    })
-})
 
-function boardcast(msg) {
-    wss.clients.forEach(c => {
-        c.send(msg)
+async function init() {
+    async function initRedis() {
+        await sub.connect()
+        await pub.connect()
+        sub.on('error', err => console.log('Redis Sub Error', err))
+        pub.on('error', err => console.log('Redis Pub Error', err))
+    }
+    await initRedis()
+
+    wss.on('connection', conn => {
+        console.log('a client just connected')
+        conn.on('message', msg => {
+            pub.publish(CHANNEL, msg.toString())
+        })
     })
+
+    sub.subscribe(CHANNEL,  msg => {
+        wss.clients.forEach(c => {
+            c.send(msg)
+        })
+    })
+
+    server.listen(process.argv[2] || 3000)
 }
 
-server.listen(process.argv[2] || 3000)
+init()
